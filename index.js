08cd9910985a71f7ebc0bee4a92c229a82ce03d0
aneeyfarms <anneeyfarms@gmail.com>
2025-06-10 12:00:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/*let hasSolvedChallenge = false
+let hasHintLeft = false
+
+if (hasSolvedChallenge === false && hasHintLeft === false){
+    showSolution()
+}
+
+function showSolution(){
+    console.log("Showing the solution...")
+
+
+}*/
+
+
+/* let likesDocumentaries = true
+let likesStartups = false
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
+}
+
+function recommendMovie(){
+    console.log("Hey, check out this new film we think you will like!")
+} */
+
+// let names = ['Ania','Dave', 'Hannah','Bob']
+// names.forEach(name => {
+//     console.log(name +' is the best!')
+// })
